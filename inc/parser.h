4f4279ebc5643105cb6debfc7a1b3567f7384754
srudman <sarika.zna@gmail.com>
2024-05-03 18:03:34 +0100

    .

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 21:38:29 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 16:17:32 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 18:01:01 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 # include "minishell.h"
 
 /* STRUCTS*/
+
+typedef struct s_cmd t_cmd;
+
+/* A part of the final command table where all the command details are stored*/
+typedef struct s_cmd
+{
+	t_cmd	*prev;
+	t_cmd	*next; // in a pipe, there will be more than one command. Default value = NULL;
+	bool	builtin; // default is false
+	// pointer to builtin struct?
+	char	**path;	// path by the environment {/usr/bin, /sbin/bin,...}. Default value = NULL;
+	char	**cmd_arg; // includes the command and flags, e.g. "cat -c" {cat, -c}. Default value = NULL;
+	char	**args; // some commands contain certain arguments, e.g. "echo "hello world"". Default value = NULL;
+	bool	skip_cmd; // in case it's already proven to be invalid. Default value = false;
+}	t_cmd;
+
 
 /* Final command table provided by the parser to the executor*/
 typedef struct s_table
@@ -28,21 +44,8 @@ typedef struct s_table
 	int		pipes; // default 0
 	char	**env; // enivronemtn needed for excve(___, ____, **env);
 	bool	env_exists; // yes/ no, default yes
-	char	heredoc; // int or char?
+	char	*heredoc; // int or char?
 	char	*prompt;
 }	t_table;
-
-/* A part of the final command table where all the command details are stored*/
-typedef struct s_cmd
-{
-	t_cmd	*prev;
-	t_cmd	*next; // in a pipe, there will be more than one command. Default value = NULL;
-	bool	builtin; // default is false
-	// pointer to builtin struct?
-	char	**path;	// path by the environment {/usr/bin, /sbin/bin,...}. Default value = NULL;
-	char	**cmd_arg; // includes the command and flags, e.g. "cat -c" {cat, -c}. Default value = NULL;
-	char	**args: // some commands contain certain arguments, e.g. "echo "hello world"". Default value = NULL;
-	bool	skip_cmd; // in case it's already proven to be invalid. Default value = false;
-}	t_cmd;
 
 #endif
