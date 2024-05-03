4220c4a90e6e85e0434243d85be15ef36d9719e1
srudman <sarika.zna@gmail.com>
2024-05-03 17:37:04 +0100

    .

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 17:12:15 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 16:35:32 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 17:34:52 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./inc/minishell.h"
+#include "../inc/minishell.h"
 
 /* The program ./minishell does not accept any arguments to that is reviewed 
 in arg_checker() function. 
@@ -23,8 +23,8 @@ int	main(int argc, char **argv, char **envp)
 	t_table data;
 	
 	arg_checker(argc, argv); // checker to run ./minishell
-	ft_putstr_fd("\nFilipe and Sara welcome you to minishell\n", STDIN_FILENO);
-	// init_data(&data);
+	ft_putstr_fd("\nFilipe and Sara welcome you to minishell\n", STDOUT_FILENO);
+	init_data(&data);
     while (1) // minisehll loop
     {
     }
--- src/utils/struct_init.c
+++ src/utils/struct_init.c
@@ -6,13 +6,45 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:48:49 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 16:30:43 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 17:34:54 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./inc/minishell.h"
+#include "../utils/inc/minishell.h"
 
-// void    init_data(t_table *data)
-// {
-    
-// }
+void    init_data(t_table *data)
+{
+	data = malloc(sizeof(t_table));
+	if (!data)
+	{
+		free(data);
+		// put error that memory failed
+	}
+	infile = STDIN_FILENO;
+	outfile = STDOUT_FILENO;
+	errfile = STDERR_FILENO;
+	infile_valid = true;
+	otufile_valid = true;
+	pipes = 0;
+	env = NULL;
+	env_exists = true;
+	heredoc = NULL;
+	prompt = NULL;
+	init_cmd(data);
+}
+
+void init_cmd(t_table *data)
+{
+	data.cmd = malloc(sizeof(t_cmd));
+	if (!data.cmd)
+	{
+		free(data.cmd);
+	}
+	data.cmd.prev = NULL;
+	data.cmd.next = NULL;
+	data.cmd.builtin = false;
+	data.cmd.path = NULL;
+	data.cmd.cmd_arg = NULL;
+	data.cmd.args = NULL;
+	data.cmd.skip_cmd = false;
+}
