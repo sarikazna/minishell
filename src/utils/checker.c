a8089ed375f97aa12f221ca63d7483b03959402c
srudman <sarika.zna@gmail.com>
2024-05-03 17:41:23 +0100

    .

--- src/utils/checker.c
+++ src/utils/checker.c
@@ -6,11 +6,11 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:48:59 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 17:34:56 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 17:41:01 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
+#include "../../inc/minishell.h"
 
 /* Checking if the person ran ./minishell without mistakes (e.g. extra
 argument). One sole argument can be used to run ./minishell */
--- src/utils/struct_init.c
+++ src/utils/struct_init.c
@@ -6,11 +6,11 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 15:48:49 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 17:34:54 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 17:41:10 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../utils/inc/minishell.h"
+#include "../../inc/minishell.h"
 
 void    init_data(t_table *data)
 {
