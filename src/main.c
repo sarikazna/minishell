f0dfae078d5dec9864baed4513f3055aa4f5ac57
srudman <sarika.zna@gmail.com>
2024-05-03 18:39:06 +0100

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 17:12:15 by srudman           #+#    #+#             */
-/*   Updated: 2024/05/03 18:33:32 by srudman          ###   ########.fr       */
+/*   Updated: 2024/05/03 18:38:53 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_table data;
 	
+	// loop inspiration> https://github.com/Alice-inNovember/42_Minishell/blob/main/srcs/main.c#L35
 	arg_checker(argc, argv); // checker to run ./minishell
 	ft_putstr_fd("\nFilipe and Sara welcome you to minishell\n", STDOUT_FILENO);
     while (1) // minisehll loop
